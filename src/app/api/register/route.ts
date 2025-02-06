c0b8090bc13958717551a2db5ee0f03352d0cf48
linax <c.lagosv@gmail.com>
2025-02-05 21:24:54 -0300

    fix route 2

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server"
 
-export async function POST(request: Request) {
+//export async function POST(request: Request) {
+export async function POST() {
   // const { email } = await request.json()
 
   try {
